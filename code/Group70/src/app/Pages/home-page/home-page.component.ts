7560100acad520579da7887ac0fa4f14e31f37fd
Joseph Vitale <jcv3m4@mail.missouri.edu>
2019-11-16 19:10:39 -0600

    made basic home page with sample movie data, installed material

--- code/Group70/src/app/Pages/home-page/home-page.component.ts
+++ code/Group70/src/app/Pages/home-page/home-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface Movie {
+  title: string;
+  director: string;
+  year: number;
+}
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +15,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
+  searchForm = new FormGroup({
+    title: new FormControl(''),
+    director: new FormControl(''),
+    year: new FormControl(''),
+  });
+
+  movies: Movie[] = [
+    {title: "Citizen Kane", director: "Orson Welles", year: 1941},
+    {title: "The Dark Knight", director: "Christopher Nolan", year: 2008},
+    {title: "The Dark Knight 2", director: "Christopher Nolan", year: 2009},
+    {title: "The Dark Knight 3", director: "Christopher Nolan", year: 2010},
+  ];
+
+  searchResults: Movie[] = this.movies;
 
   ngOnInit() {
   }
 
+  search(){
+    this.movies.forEach(function(movie: Movie){
+       console.log("hi");
+    });
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.searchForm.value);
+    this.searchResults = [];
+  }
+  selectFilm(){
+    window.alert("hi");
+  }
+
 }
+
--- code/Group70/src/app/app.module.ts
+++ code/Group70/src/app/app.module.ts
@@ -12,6 +12,20 @@ import { environment } from '../environments/environment';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,6 +39,18 @@ import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.com
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatExpansionModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
