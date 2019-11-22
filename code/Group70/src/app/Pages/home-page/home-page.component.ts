b9d8937869fff9a589dd1fb975d3ec6787cc25ba
Joseph Vitale <jcv3m4@mail.missouri.edu>
2019-11-22 17:54:17 -0600

    worked with service

--- code/Group70/src/app/Pages/home-page/home-page.component.ts
+++ code/Group70/src/app/Pages/home-page/home-page.component.ts
@@ -1,17 +1,20 @@
 import { Component} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { Observable } from 'rxjs';
+import { Observable, Subject} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import { PreviewComponent } from '../preview/preview.component';
+import { TmdbServiceService } from '../../Services/tmdb-service.service';
+import { CloseScrollStrategy } from '@angular/cdk/overlay';
 
 //will replace when all code merged
 export interface Movie {
   id: number,
   title: string,
+  genre_ids: number[],
+  poster: string,
   description: string,
-  releaseDate: string,
-  posterSrc: string
+  release_date: string
 }
 
 @Component({
@@ -26,31 +29,47 @@ export class HomePageComponent{
     genre: new FormControl(''),
   });
   movies: Movie[] = [
-    {title: "Citizen Kane", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
-    {title: "Citizen Kane 2", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
-    {title: "Citizen Kane 3", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
-    {title: "Citizen Kane 4", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
-    {title: "Citizen Kane 5", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
-    {title: "Citizen Kane 6", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
-    {title: "Citizen Kane 7", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
-    {title: "Citizen Kane 8", id:0, description:"sample description.", releaseDate:"1941", posterSrc:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg"},
+    {title: "Citizen Kane", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
+    {title: "Citizen Kane 2", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
+    {title: "Citizen Kane 3", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
+    {title: "Citizen Kane 4", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
+    {title: "Citizen Kane 5", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
+    {title: "Citizen Kane 6", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
+    {title: "Citizen Kane 7", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
+    {title: "Citizen Kane 8", id:0, description:"sample description.", release_date:"1941", poster:"https://images-na.ssl-images-amazon.com/images/I/81AJdOIEIhL._SL1500_.jpg", genre_ids: []},
   ];
   searchResults: Movie[];
 
-  constructor(public dialog: MatDialog) { 
+
+  constructor(
+    public dialog: MatDialog,
+    public movieService: TmdbServiceService,
+    ) { 
     this.searchResults = [];
   }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
-    console.warn(this.searchForm.value);
+    //console.log(this.searchForm.value);
     this.searchResults = this.movies;
+  }
+
+  searchPopular(){
+    this.searchResults=[];
+    //console.log("popular movies: ",this.movieService.getPopular());
+    let res = new Subject();
+    res = this.movieService.getPopular();
+    console.log("res: ",res);
+    res.subscribe({
+      next: (v : Movie[]) => this.searchResults = v
+      
+    });
   }
 
   openPreview(movie:Movie): void {
     const dialogRef = this.dialog.open(PreviewComponent, {
       width: '500px',
-      data: {title: movie.title, id: movie.id, releaseDate: movie.releaseDate}
+      data: {title: movie.title, id: movie.id, releaseDate: movie.release_date}
     });
   }
 }
