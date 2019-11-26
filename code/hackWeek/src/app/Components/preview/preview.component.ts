6b960656d48430b048abf3babe226a404d1a973d
Jingsong Tan <jtnfx@mail.missouri.edu>
2019-11-26 00:51:11 -0600

    homepage work

--- code/hackWeek/src/app/Components/preview/preview.component.ts
+++ code/hackWeek/src/app/Components/preview/preview.component.ts
@@ -15,26 +15,23 @@ export class PreviewComponent implements OnInit {
   constructor(
     public reviewService: ReviewService,
     public dialogRef: MatDialogRef<PreviewComponent>,
-    @Inject(MAT_DIALOG_DATA) public movie: Movie
-    ) {
-
+    @Inject(MAT_DIALOG_DATA) private data: any
+  ) {
+    this.movie = data.movie;
   }
 
-  reviews: Reviews;
+  movie: Movie;
+  reviews: Review[] = [];
   /*[{user: "bob", score: 5, date: "2019-11-24", title: "good film!", comment: "I liked this film", replies: [] },
     {user: "bob2", score: 5, date: "2019-11-24", title: "good film!", comment: "I liked this film", replies: [] },
     {user: "bob3", score: 5, date: "2019-11-24", title: "good film!", comment: "I liked this film", replies: [] },
     {user: "bob4", score: 5, date: "2019-11-24", title: "good film!", comment: "I liked this film", replies: [] },
     {user: "bob5", score: 5, date: "2019-11-24", title: "good film!", comment: "I liked this film", replies: [] }
   ];*/
-  review: Review;
 
   ngOnInit() {
-    this.reviews = {reviews: []};
-    this.reviewService.retrieveMovieReviews(this.movie.id.toString())
-    .subscribe(reviews =>{
-      this.reviews = reviews as Reviews;
-      console.log(this.reviews);
+    this.reviewService.retrieveMovieReviews(this.movie.id.toString()).subscribe(reviews =>{
+      this.reviews = reviews.reviews;
     })
   }
 
